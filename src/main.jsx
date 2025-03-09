060423fa374c77ea39c3909412bb3508a3ff2702
Mohamed Hazem <gggftyyfd@gmail.com>
2025-03-09 16:16:25 +0200

    add basename in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/My-Profile">
     <App />
   </BrowserRouter>,
 )
