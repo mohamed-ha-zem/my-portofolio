299673dda41d039a081364725fb8b726c89e8c91
Mohamed Hazem <gggftyyfd@gmail.com>
2025-04-30 16:01:05 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/my-portofolio">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portofolio/",
+  base: '/', // لـ Netlify
 });
-
-
-
